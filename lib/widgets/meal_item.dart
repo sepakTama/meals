041406c2699bb568da3a7cf31fd1dda7bb14b569
masaaki.tamai <ma.tama121096@gmail.com>
2024-02-23 23:45:37 +0900

    add new page

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -7,9 +7,11 @@ class MealItem extends StatelessWidget {
   const MealItem({
     super.key,
     required this.meal,
+    required this.onSelectMeal,
   });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String get complexityText {
     // first letter is changed to upper case
@@ -32,7 +34,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
