3c879f4b52e71bdc87a6f67faaf4649cd29938e5
masaaki.tamai <ma.tama121096@gmail.com>
2024-02-25 16:12:43 +0900

    add drawer on FiltersScreen

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/tabs.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -18,6 +20,16 @@ class _FiltersScreen extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
+      drawer: MainDrawer(onSelectScreen: (identifier) {
+        Navigator.of(context).pop();
+        if (identifier == 'meals') {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => const TabsScreen(),
+            ),
+          );
+        }
+      }),
       body: Column(
         children: [
           SwitchListTile(
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -50,15 +50,13 @@ class _TabsScreenState extends State<TabsScreen> {
   }
 
   void _setScreen(String indentifier) {
+    Navigator.of(context).pop();
     if (indentifier == 'filters') {
-      Navigator.of(context).pop();
       Navigator.of(context).push(
         MaterialPageRoute(
           builder: (ctx) => const FiltersScreen(),
         ),
       );
-    } else {
-      Navigator.of(context).pop();
     }
   }
 
